183aa5a1d9cf994a713375e93b215dd6c9ac50ec
kata0321 <tpgare3@gmail.com>
2024-03-18 09:34:49 -0700

    task1

--- 0x0F-python-object_relational_mapping/0-select-states.py
+++ 0x0F-python-object_relational_mapping/0-select-states.py
@@ -2,6 +2,8 @@
 """ lists all states from the database hbtn_0e_0_usa """
 import MysQLdb
 import sys
+
+if __name__ = "__main__":
 db=_mysql.connect(host="localhost",user="sys.agrv[1]",
                   password="sys.agrv[2]",database="sys.agrv.[3]",port="3306")
 c = db.cursor()
