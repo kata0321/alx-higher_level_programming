19898359fbadc8ef20f33a85cef7166e1e079220
kata0321 <tpgare3@gmail.com>
2024-04-21 11:15:16 -0700

    task4

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,3 +1,5 @@
--- Create table in my database
+-- Creates a table in the current database
 -- Query to create a table called first_table in the current database in your MySQL server
-CREATE TABLE IF NOT EXIST first_table (id INT,name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS first_table (
+id INT,
+name VARCHAR(256));
