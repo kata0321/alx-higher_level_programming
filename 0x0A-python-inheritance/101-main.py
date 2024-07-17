579acd635afb267048885db4ffa34a5f911f6cd4
kata0321 <tpgare3@gmail.com>
2024-07-17 11:31:13 -0700

    rest

---
+++ 0x0A-python-inheritance/101-main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+add_attribute = __import__('101-add_attribute').add_attribute
+
+class MyClass():
+    pass
+
+mc = MyClass()
+add_attribute(mc, "name", "John")
+print(mc.name)
+
+try:
+    a = "My String"
+    add_attribute(a, "name", "Bob")
+    print(a.name)
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+
+guillaume@ubuntu:~/0x0A$ ./101-main.py
+John
+[TypeError] can't add new attribute
